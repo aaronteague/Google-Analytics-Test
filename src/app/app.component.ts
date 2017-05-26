80a8f90f23d9fc72e4bd98cb6e8a3cc5d0980a25
Aaron Teague <aaron.teague@outlook.com>
2017-05-26 16:39:06 -0500

    moved google analytics to the index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 ///<reference path="app.component.d.ts" />
 import { Component } from '@angular/core';
+//import { Angulartics2GoogleAnalytics } from 'angulartics2';
+
 
 
 @Component({
@@ -19,16 +21,8 @@ export class AppComponent {
   } 
 
   initGoogleAnalytics(){
-  var currdate : any = new Date();
-  
-  
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*currdate;a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-100029182-1', 'auto');
-  ga('send', 'pageview');
+
 
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+//import { Angulartics2GoogleAnalytics,Angulartics2Module } from 'angulartics2';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule
+  //  Angulartics2Module.forRoot([Angulartics2GoogleAnalytics])
   ],
   providers: [],
   bootstrap: [AppComponent]
