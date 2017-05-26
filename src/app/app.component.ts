c070838f378da9beb392c68467c74e9cf6015787
Aaron Teague <aaron.teague@outlook.com>
2017-05-26 12:03:20 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+///<reference path="app.component.d.ts" />
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+
+export class AppComponent {
+  title = 'app works!';
+
+
+  AppComponent(){
+    this.initGoogleAnalytics();
+  } 
+
+  initGoogleAnalytics(){
+  var currdate : any = new Date();
+  
+  
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*currdate;a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-100029182-1', 'auto');
+  ga('send', 'pageview');
+
+  }
+}
